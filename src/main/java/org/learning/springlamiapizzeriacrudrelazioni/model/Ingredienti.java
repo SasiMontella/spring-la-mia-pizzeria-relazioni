a950c2f15b3f0d39e4e7094a88482d8ee711b351
Sasi2294 <montella.salvatore@live.it>
2024-01-18 14:35:46 +0100

    Aggiunto model Ingredienti e relativo Repository

---
+++ src/main/java/org/learning/springlamiapizzeriacrudrelazioni/model/Ingredienti.java
@@ -0,0 +1,34 @@
+package org.learning.springlamiapizzeriacrudrelazioni.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Ingredienti {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Integer id;
+@NotEmpty
+@Size(max = 100)
+private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/org/learning/springlamiapizzeriacrudrelazioni/repository/IngredientiRepository.java
@@ -0,0 +1,7 @@
+package org.learning.springlamiapizzeriacrudrelazioni.repository;
+
+import org.learning.springlamiapizzeriacrudrelazioni.model.Ingredienti;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IngredientiRepository extends JpaRepository<Ingredienti, Integer> {
+}
